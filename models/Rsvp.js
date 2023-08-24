213ed6a59dda0f67dcac047b0f17c9ed55abfda7
marvs000 <marvinbaste000@gmail.com>
2023-08-24 16:38:01 +0800

    Dedication Version Initial Commit

--- models/Rsvp.js
+++ models/Rsvp.js
@@ -4,8 +4,7 @@ const sequelize = require('../config/connection');
 const Rsvp = sequelize.define('Rsvp', {
   // Model attributes are defined here
   complete_name: { allowNull: false, type: DataTypes.STRING },
-  confirmed: { allowNull: false, type: DataTypes.STRING },
-  suggestions: { allowNull: true, type: DataTypes.STRING },
+  confirmed: { allowNull: false, type: DataTypes.STRING }
 }, {
   sequelize,
   modelName: 'Rsvp',
